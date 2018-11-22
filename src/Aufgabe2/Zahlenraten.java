1e7e061600cf91f5c1199f7d87605283263c5c96
unknown <you@example.com>
2018-11-22 18:17:19 +0100

    Aufgabe 2 fertig

--- src/Aufgabe2/Zahlenraten.java
+++ src/Aufgabe2/Zahlenraten.java
@@ -1,5 +1,6 @@
 package Aufgabe2;
 
+import javax.swing.*;
 import java.util.Random;
 
 public class Zahlenraten {
@@ -8,4 +9,21 @@ public class Zahlenraten {
 
     Random rnd = new Random();
     int zufallszahl = rnd.nextInt(max - min + 1) + min;
+
+    /*Erzeugen einer Zufallszahl zwischen 1 und 10
+         Der Standardweg dafür ist wie folgt:
+         Zur Verfügung stellen:
+            -min Mindestwert
+            -max Maximalwert
+          und erhalten im Gegenzug eine ganze Zahl zwischen min und max inklusive
+     */
+
+    {
+        if ((zufallszahl % 2) == 0) {            // Zahl ist gerade
+            JOptionPane.showMessageDialog(null, "Die Zahl ist gerade!");
+
+        } else {                                // Zahl ist ungerade
+            JOptionPane.showMessageDialog(null, "Die Zahl ist ungerade!");
+        }
+    }
 }
